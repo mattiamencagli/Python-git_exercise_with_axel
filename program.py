295d965a8e06e37439aeaa29d8ff96e897ec393d
Mattia Mencagli <mattiamencagli@hotmail.it>
2021-01-26 18:57:51 +0100

    polynomial functions 2 and 3 implemented

--- program.py
+++ program.py
@@ -15,9 +15,19 @@ xval = np.linspace(start,stop,int((stop-start)/step)+1);
 
 def f1(x):
 	return x
+
+def f2(x):
+	return x**2
+
+def f3(x):
+	return x**3
 	
 if func_switch==1:
 	yval = f1(xval)
+elif func_switch==2:
+	yval = f2(xval)
+elif func_switch==3:
+	yval = f3(xval)
 
 
 #plot lists
