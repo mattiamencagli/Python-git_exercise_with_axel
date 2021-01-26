99cbcc0b9c1f56eaf7a1456c6e5ec2a70462a9c9
Mattia Mencagli <mattiamencagli@hotmail.it>
2021-01-26 18:53:06 +0100

    fill lists

--- program.py
+++ program.py
@@ -1,5 +1,18 @@
 import sys
-
+import numpy as np
 
 #handle command line
 func_switch = int(sys.argv[1])
+
+
+#fill lists
+start=-5
+stop=5
+step=0.1
+xval = np.linspace(start,stop,int((stop-start)/step)+1);
+
+def f1(x):
+	return x
+	
+if func_switch==1:
+	yval = f1(xval)
